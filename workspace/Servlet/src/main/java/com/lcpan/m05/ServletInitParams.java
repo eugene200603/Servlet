54d9d3dfc3ed4d0ac2ba92abc977f2c3aa9affb1
eugene200603 <eugene200403@gmail.com>
2023-03-06 16:35:48 +0800

    2023/3/6

--- workspace/Servlet/src/main/java/com/lcpan/m05/ServletInitParams.java
+++ workspace/Servlet/src/main/java/com/lcpan/m05/ServletInitParams.java
@@ -11,6 +11,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet(urlPatterns="/ServletInitParams",
+			initParams= {@WebInitParam(name="greeting", value="Have a nice day!"),
+					@WebInitParam(name="count", value="3")})
+
 public class ServletInitParams extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	String servletName;
